75f7502e8eb5ca08c790e59708520a40492b64e3
Poletipo <tijimjim@hotmail.com>
2021-02-11 14:22:29 -0500

    init commit

---
+++ Assets/Scripts/GridCube.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridCube : MonoBehaviour
+{
+    public int xSize, ySize, zSize = 3;
+    public float gridSize = 1;
+    Vector3[] vertices;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Generate();
+    }
+    
+    void Generate() {
+        vertices = new Vector3[(xSize + 1) * (ySize + 1) * (zSize + 1)];
+        int i = 0;
+        for (int z = 0; z < zSize; z++) {
+            for (int y = 0; y < ySize; y++) {
+                for (int x = 0; x < xSize; x++) {
+                    vertices[i] = new Vector3(x, y, z);
+                    i++;
+                }
+            }
+        }
+    }
+
+    private void OnDrawGizmos() {
+        if (vertices == null) {
+            return;
+        }
+
+        Gizmos.color = Color.black;
+        for (int i = 0; i < vertices.Length; i++) {
+            Gizmos.DrawSphere(vertices[i], 0.1f);
+        }
+    }
+
+
+    struct verticesValue {
+        Vector3 vertice;
+        float value;
+    }
+}
---
+++ Assets/Shader/NewUnlitShader.shader
@@ -0,0 +1,18 @@
+﻿Shader "Test/TestShader"{
+	Properties{
+		_Color("Color", Color) = (0,0,0,1)
+	}
+
+	SubShader{
+
+		Pass{
+
+			Material{
+				Diffuse[_Color]
+			}
+			Lighting On
+
+		}
+
+	}
+}
