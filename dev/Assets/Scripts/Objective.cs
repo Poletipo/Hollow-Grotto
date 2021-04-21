d96e6753d75b93836035456b018d325d5916306b
Poletipo <tijimjim@hotmail.com>
2021-04-20 22:00:29 -0400

    Created Sonar particle

--- dev/Assets/Scripts/Objective.cs
+++ dev/Assets/Scripts/Objective.cs
@@ -1,18 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Objective : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+public class Objective : MonoBehaviour {
+    public GameObject Sonar;
+
+    bool Active = true;
+
+    private void Update()
     {
-        
+
+        float dist = Vector3.Distance(transform.position, GameManager.Instance.Player.transform.position);
+        float SonarSize = Mathf.Clamp((dist / 50.0f), 1, 9999999999999999);
+
+
+        Sonar.transform.localScale = Vector3.one * SonarSize;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
